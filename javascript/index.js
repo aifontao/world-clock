f9fe47f302687effc90a2be33eabe619acf0d7b9
aifontao <anaisabelfontao@gmail.com>
2023-11-23 22:24:58 +0000

    Added more cities to the world clock

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,46 @@
 function updateTime() {
-  //Los Angeles
-  let losAngelesElement = document.querySelector("#los-angeles");
-  if (losAngelesElement) {
-    let losAngelesDateElement = losAngelesElement.querySelector(".date");
-    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-    let losAngelesTime = moment().tz("America/Los_Angeles");
-    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+  //New York
+  let newYorkElement = document.querySelector("#new-york");
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+    newYorkTimeElement.innerHTML = newYorkTime.format(
+      `HH:mm:ss [<small>]A[</small>]`
+    );
+  }
+  //Cape Verde
+  let capeVerdeElement = document.querySelector("#cape-verde");
+  if (capeVerdeElement) {
+    let capeVerdeDateElement = capeVerdeElement.querySelector(".date");
+    let capeVerdeTimeElement = capeVerdeElement.querySelector(".time");
+    let capeVerdeTime = moment().tz("America/Los_Angeles");
+    capeVerdeDateElement.innerHTML = capeVerdeTime.format("MMMM Do YYYY");
+    capeVerdeTimeElement.innerHTML = capeVerdeTime.format(
+      `HH:mm:ss [<small>]A[</small>]`
+    );
+  }
+  //London
+  let londonElement = document.querySelector("#london");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
+    londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+    londonTimeElement.innerHTML = londonTime.format(
       `HH:mm:ss [<small>]A[</small>]`
     );
   }
 
-  //Paris
-  let parisElement = document.querySelector("#paris");
-  if (parisElement) {
-    let parisDateElement = parisElement.querySelector(".date");
-    let parisTimeElement = parisElement.querySelector(".time");
-    let parisTime = moment().tz("Europe/Paris");
-    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-    parisTimeElement.innerHTML = parisTime.format(
+  //Shangai
+  let shangaiElement = document.querySelector("#shanghai");
+  if (shangaiElement) {
+    let shangaiDateElement = shangaiElement.querySelector(".date");
+    let shangaiTimeElement = shangaiElement.querySelector(".time");
+    let shangaiTime = moment().tz("Asia/Shanghai");
+    shangaiDateElement.innerHTML = shangaiTime.format("MMMM Do YYYY");
+    shangaiTimeElement.innerHTML = shangaiTime.format(
       `HH:mm:ss [<small>]A[</small>]`
     );
   }
